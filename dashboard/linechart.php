e0f03b39ef27bd607dcd60a9df9f66e824dc75f3
sarvon77 <sarvon77@hotmail.com>
2014-01-10 13:02:01 +0530

    version
    1.1

--- dashboard/linechart.php
+++
@@ -1,7 +0,0 @@
-<li class="span4" id="linechart.php">
-<div class="thumbnail" id="thumbnail4" style="padding:0px">
-				<div class="demo-container">
-					<div id="placeholder" class="demo-placeholder"></div>
-				</div>
-				</div>
-				</li>
--- dashboard/outlock.php
+++
@@ -1,5 +0,0 @@
- <li class="span8" id="outlock.php">
-<div class="thumbnail" id="thumbnail2" >
-					<p style="margin: 0;"><b>Outlook</b><span class="pull-right" ><i id="outlook" class="glyphicon glyphicon-plus-sign"></i></span></p>
-				</div>
-			</li>
